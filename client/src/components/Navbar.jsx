57bbb9a82d436ccffc081afaa42cb19a30e39777
Dineth Lochana Mallawarachchi <30839724+dineth-lochana@users.noreply.github.com>
2025-02-23 09:14:22 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { auth } from '../firebaseConfig';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 
